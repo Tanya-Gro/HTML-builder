a5f8768b01a85a990f03675b6efcce7e3af5ea3a
unknown <tarpel.gta@gmail.com>
2025-01-22 00:20:00 +0200

    feat: 05 merge style

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const folderFrom = path.join(__dirname, 'styles');
+const fileInto = path.join(__dirname, 'project-dist', 'bundle.css');
+
+(async function () {
+  try {
+    //читаем папку с файлами стилей
+    const styles = await fs.readdir(folderFrom, { withFileTypes: true });
+    //создаем бардлер
+    await fs.writeFile(fileInto, '', 'utf8');
+    styles.forEach(async (file) => {
+      //игнорируем не файлы стилей
+      if (file.isFile() && path.extname(file.name) === '.css') {
+        //записываем в бандлер
+        await fs.appendFile(
+          fileInto,
+          await fs.readFile(`${folderFrom}\\${file.name}`,
+            {encoding: 'utf8'}));
+      }
+    });
+    console.log('bundle was created successfully');
+  } catch (err) {
+    console.error('bundle wasn`t created:', err);
+  }
+})();
