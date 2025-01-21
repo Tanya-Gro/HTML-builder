3e0477b974df1b5a3b19389987e4e6a2fe5ef9b2
unknown <tarpel.gta@gmail.com>
2025-01-22 00:40:42 +0200

    fix: 02 write file fix farewell phrase

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,13 +4,15 @@ const path = require('path');
 const writeFile = path.join(__dirname, 'input.txt');
 const writable = fs.createWriteStream(writeFile);
 
-process.stdout.write('Enter the text or "q" for exit\n');
+process.stdout.write('Enter the text or "EXIT" to finish\n');
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
-  input === 'q' || input === 'Q' ? exit() : writable.write(input + '\n');
+  input.toLowerCase() === 'exit' ? exit() : writable.write(input + '\n');
 });
 
 function exit() {
-  process.stdout.write('See you)\n');
+  process.stdout.write('Good luck!\n');
   process.exit();
 }
+
+process.on('SIGINT', exit);
---
+++ 06-build-page/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderFrom = path.join(__dirname, 'styles');
+const fileInto = path.join(__dirname, 'project-dist', 'bundle.css');
+
+(function () {
+  
+})();
