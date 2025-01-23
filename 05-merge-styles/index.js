0598935ad126954076d603d2d6868bdc895d72c3
unknown <tarpel.gta@gmail.com>
2025-01-24 00:14:22 +0200

    fix: 05 mistakes by building

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 
 const folderFrom = path.join(__dirname, 'styles');
@@ -6,22 +6,24 @@ const fileInto = path.join(__dirname, 'project-dist', 'bundle.css');
 
 (async function () {
   try {
-    //читаем папку с файлами стилей
+    // Читаем папку с файлами стилей
     const styles = await fs.readdir(folderFrom, { withFileTypes: true });
-    //создаем бардлер
+
+    // Создаем (или очищаем) бандл
     await fs.writeFile(fileInto, '', 'utf8');
-    styles.forEach(async (file) => {
+
+    // Обрабатываем файлы последовательно
+    for (const file of styles) {
       //игнорируем не файлы стилей
       if (file.isFile() && path.extname(file.name) === '.css') {
-        //записываем в бандлер
-        await fs.appendFile(
-          fileInto,
-          await fs.readFile(`${folderFrom}\\${file.name}`,
-            {encoding: 'utf8'}));
+        const filePath = path.join(folderFrom, file.name); // Полный путь к CSS-файлу
+        const cssContent = await fs.readFile(filePath, 'utf8'); // Чтение файла
+        await fs.appendFile(fileInto, cssContent, 'utf8'); // Добавление содержимого в бандл
       }
-    });
+    }
+
     console.log('bundle was created successfully');
   } catch (err) {
-    console.error('bundle wasn`t created:', err);
+    console.error('bundle wasn`t created: ', err);
   }
 })();
